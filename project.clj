896d88664f8ef0c6cb559acdeff680ddcdfaf1c9
Jan Stępień <jan.stepien@stylefruits.de>
2013-11-08 11:20:50 +0100

    Bump dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject stylefruits/gniazdo "0.0.0-SNAPSHOT"
   :description "A WebSocket client for Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.eclipse.jetty.websocket/websocket-client "9.0.4.v20130625"]]
+                 [org.eclipse.jetty.websocket/websocket-client "9.1.0.RC1"]]
   :repl-options {:init-ns gniazdo.core}
   :jvm-opts ["-Dorg.eclipse.jetty.websocket.client.LEVEL=WARN"]
   :profiles {:dev
-             {:dependencies [[http-kit "2.1.4"]]}})
+             {:dependencies [[http-kit "2.1.13"]]}})
