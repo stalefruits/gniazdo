7f96a85b60a80da00a2a2e08025f0a3ef3b84701
Jan Stępień <jan.stepien@stylefruits.de>
2015-10-02 11:11:45 +0200

    Bump the version to 1.0.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stylefruits/gniazdo "0.4.1"
+(defproject stylefruits/gniazdo "1.0.0-SNAPSHOT"
   :description "A WebSocket client for Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.eclipse.jetty.websocket/websocket-client "9.3.3.v20150827"]]
