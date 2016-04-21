143e6cb458a7eb602a96adf822ae011e9a17fea5
Jan Stępień <jan.stepien@stylefruits.de>
2016-04-21 14:18:41 +0200

    Update org.eclipse.jetty.websocket/websocket-client

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject stylefruits/gniazdo "1.0.0-SNAPSHOT"
   :description "A WebSocket client for Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.eclipse.jetty.websocket/websocket-client "9.3.3.v20150827"]]
+                 [org.eclipse.jetty.websocket/websocket-client "9.3.8.v20160314"]]
   :repl-options {:init-ns gniazdo.core}
   :jvm-opts ["-Dorg.eclipse.jetty.websocket.client.LEVEL=WARN"]
   :profiles {:dev
