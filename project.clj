c1c83d3c26b44dbfd0534ed2224dedec5d6bb890
Jan Stępień <jan.stepien@innoq.com>
2018-11-22 11:57:11 +0100

    Bump websocket-client to 9.4.14

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :author "stylefruits GmbH"}
   :url "https://github.com/stylefruits/gniazdo"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.eclipse.jetty.websocket/websocket-client "9.4.12.v20180830"]]
+                 [org.eclipse.jetty.websocket/websocket-client "9.4.14.v20181114"]]
   :repl-options {:init-ns gniazdo.core}
   :jvm-opts ["-Dorg.eclipse.jetty.websocket.client.LEVEL=WARN"]
   :profiles {:dev
