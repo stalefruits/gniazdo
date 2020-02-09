ef481bdf866c7e58c12a9684cc0ab753b01fde4f
Jan Stępień <jan.stepien@innoq.com>
2020-02-09 11:40:54 +0100

    Version 1.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stylefruits/gniazdo "1.1.2"
+(defproject stylefruits/gniazdo "1.1.3"
   :description "A WebSocket client for Clojure"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"
